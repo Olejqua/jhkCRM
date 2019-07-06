6076546b6df87c5ef7a8d200620762451feaae88
d.s.pustovit <d.s.pustovit@m9.etagi.com>
2019-07-06 18:09:58 +0500

    add axios

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,6 +1,19 @@
 import { h } from 'preact';
 import style from './style';
 
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('http://localhost:3000/admin')
+	.then(function (response) {
+		console.log(response);
+	})
+	.catch(function (error) {
+		console.log(error);
+	})
+	.finally(function () {
+	});
+
 const Home = () => (
 	<div class={style.home}>
 		<h1>Home</h1>
